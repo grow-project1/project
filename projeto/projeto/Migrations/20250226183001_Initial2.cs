4871126a89f2d78469f5d58745732d727afb6873
Rodrigo-Baiao <127238247+Rodrigo-Baiao@users.noreply.github.com>
2025-02-26 19:45:02 +0000

    Fazer Licitacaoo

---
+++ projeto/projeto/Migrations/20250226183001_Initial2.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projeto.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Licitacoes_Leiloes_LeilaoId",
+                table: "Licitacoes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LeilaoId",
+                table: "Licitacoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UtilizadorId",
+                table: "Licitacoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licitacoes_UtilizadorId",
+                table: "Licitacoes",
+                column: "UtilizadorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Licitacoes_Leiloes_LeilaoId",
+                table: "Licitacoes",
+                column: "LeilaoId",
+                principalTable: "Leiloes",
+                principalColumn: "LeilaoId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Licitacoes_Utilizador_UtilizadorId",
+                table: "Licitacoes",
+                column: "UtilizadorId",
+                principalTable: "Utilizador",
+                principalColumn: "UtilizadorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Licitacoes_Leiloes_LeilaoId",
+                table: "Licitacoes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Licitacoes_Utilizador_UtilizadorId",
+                table: "Licitacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Licitacoes_UtilizadorId",
+                table: "Licitacoes");
+
+            migrationBuilder.DropColumn(
+                name: "UtilizadorId",
+                table: "Licitacoes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LeilaoId",
+                table: "Licitacoes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Licitacoes_Leiloes_LeilaoId",
+                table: "Licitacoes",
+                column: "LeilaoId",
+                principalTable: "Leiloes",
+                principalColumn: "LeilaoId");
+        }
+    }
+}
--- projeto/projeto/Models/Licitacao.cs
+++ projeto/projeto/Models/Licitacao.cs
@@ -3,7 +3,12 @@
     public class Licitacao
     {
         public int LicitacaoId { get; set; }
-        public DateTime DataLicitacao { get; set; }
+        public DateTime DataLicitacao { get; set; } = DateTime.Now;
         public double ValorLicitacao { get; set; }
+        public int LeilaoId { get; set; }
+        public Leilao Leilao { get; set; } = default!;
+
+        public int UtilizadorId { get; set; }
+        public Utilizador Utilizador { get; set; } = default!;
     }
 }
