c526357f045786eeb7b151013bf508744cc00805
isidoroforo <202200147@estudantes.ips.pt>
2025-04-05 22:42:21 +0100

    path certo

--- projeto/growIntegrationTests/CustomWebApplicationFactory .cs
+++ projeto/growIntegrationTests/CustomWebApplicationFactory .cs
@@ -10,7 +10,7 @@ namespace growIntegrationTests
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             // Define o caminho correto para o ContentRoot
-            var contentRootPath = @"C:\3ºano\PV\projeto\project\projeto\projeto"; // Caminho correto do projeto web
+            var contentRootPath = AppContext.BaseDirectory;
 
             // Verificar se o caminho existe
             if (!System.IO.Directory.Exists(contentRootPath))
@@ -22,7 +22,6 @@ namespace growIntegrationTests
 
             builder.ConfigureServices(services =>
             {
-                // Aqui podes adicionar mocks ou outras configurações para o teste, se necessário
             });
         }
     }
